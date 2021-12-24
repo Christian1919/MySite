93659366d34746f8671d81fb067b62eb8abc1752
Christian1919 <93383929+Christian1919@users.noreply.github.com>
2021-12-24 02:34:24 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+// Cursor
+
+
+let cursor = document.getElementById('cursor')
+document.addEventListener('mousemove',function(e){
+    var x = e.clientX
+    var y = e.clientY
+    cursor.style.left = x + 'px'
+    cursor.style.top = y + 'px'
+})
+
+
+
+//Color 
+var colorWell = ""
+var defaultColor = "#c0392b"
+
+window.addEventListener("load", startup, false)
+function startup() {
+    colorWell = document.querySelector("#color")
+    colorWell.value = defaultColor
+    colorWell.addEventListener("input", updateFirst, false)
+    colorWell.addEventListener("change", updateAll, false)
+    colorWell.select()
+  }
+  function updateFirst(event) {
+    var p = document.querySelector("h3")
+    var h = document.querySelector(".information")
+    if (p,h) {
+      p.style.color = event.target.value
+      h.style.color = event.target.value
+    }
+  }
+  function updateAll(event) {
+    document.querySelectorAll("h3").forEach(function(p) {
+      p.style.color = event.target.value
+    })
+      document.querySelectorAll(".information").forEach(function(v) {
+        v.style.color = event.target.value
+      })
+        document.querySelectorAll("#cursor").forEach(function(h) {
+          h.style.background = event.target.value
+        })
+          document.querySelectorAll("header").forEach(function(h) {
+            h.style.background = event.target.value
+          })
+            document.querySelectorAll("nav").forEach(function(h) {
+              h.style.background = event.target.value
+            })
+  }
+
